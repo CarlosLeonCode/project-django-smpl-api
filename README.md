6476a03199db19d858c88337be80474fa006b410
carlosleoncode <carlosestebanleonpinilla@gmail.com>
2024-06-26 17:30:51 -0500

    feat: readme added and comments

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installation Guide
+
+```shell
+python manage.py migrate
+python manage.py makemigrations
+
+python manage.py createsuperuser
+python manage.py runserver
+```
--- api/services/save_movie_from_omdb_service.py
+++ api/services/save_movie_from_omdb_service.py
@@ -2,7 +2,6 @@ from api.models.rating import Rating
 from api.serializers.movie_serializer import MovieSerializer
 from api.libs.omdb_api_client import OMDbApiClient
 from rest_framework import status
-import pdb
 
 class SaveMovieFromOmdbService:
   omdb_client = OMDbApiClient()
@@ -17,35 +16,28 @@ class SaveMovieFromOmdbService:
         
         # keys to lowercase and "N/A" values to None
         movie_data = dict((key.lower(), (None if value == "N/A" else value)) for key, value in movie_data.items())
-
         serializer = MovieSerializer(data=movie_data)
 
+        # Saving data
         if serializer.is_valid():
           serializer.save()
-
           if ratings:
             for rating in ratings:
               data = dict((key.lower(), value) for key, value in rating.items())
               data['movie_id'] = serializer.data["id"]
               Rating.objects.create(**data)
-          
+          # Response
           data_response = [
             ratings,
             serializer.data
           ]
-          
           return data_response
         else:
-          print('---')
-          print(serializer.errors)
           error_message = {'error': 'Error saving movie data.'}
           return error_message, status.HTTP_500_INTERNAL_SERVER_ERROR
       else:
         error_message = {'error': 'Movie not found in OMDb API.'}
         return error_message, status.HTTP_404_NOT_FOUND
-
     except ValueError as e:
-      print('---')
-      print(e)
       error_message = {'error': 'Process Error'}
       return error_message, status.HTTP_500_INTERNAL_SERVER_ERROR
